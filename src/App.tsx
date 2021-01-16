109efa0fd514237b0be782e5fe4f0404f7cb7b1a
RAJAT VIJAY <rajatvijay5@gmail.com>
2021-01-16 12:24:27 +0530

    added presistance to cart state

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import "./App.css";
 import { Router } from "@reach/router";
 import { Provider } from "react-redux";
-import { store } from "./common/store";
+import { persistor, store } from "./common/store";
 import { Home } from "./pages/home";
+import { PersistGate } from "redux-persist/integration/react";
+import { Spin } from "antd";
 
 function App() {
   return (
     <Provider store={store}>
-      <Router>
-        <Home path="/" />
-      </Router>
+      <PersistGate loading={<Spin />} persistor={persistor}>
+        <Router>
+          <Home path="/" />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
--- src/common/store.ts
+++ src/common/store.ts
@@ -7,6 +7,14 @@ import {
 import { cartReducer } from "../reducers/cartSlice";
 import { productsReducer } from "../reducers/productsSlice";
 import { userReducer } from "../reducers/userSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
 
 const rootReducer = combineReducers({
   products: productsReducer,
@@ -14,9 +22,12 @@ const rootReducer = combineReducers({
   cart: cartReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
 });
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
